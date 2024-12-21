babd154f093ec19edad98054ab13751359d04518
Pauline Umunyana <pauline.umunyana@moyotech.solutions>
2024-12-21 16:13:38 +0200

    adding server

--- com.myorg.myapp/webapp/controller/Main.controller.js
+++ com.myorg.myapp/webapp/controller/Main.controller.js
@@ -1,8 +1,15 @@
-sap.ui.define([	"sap/ui/core/mvc/Controller",	"sap/m/MessageToast"], function(Controller, MessageToast) {
+sap.ui.define([	"sap/ui/core/mvc/Controller",	"sap/m/MessageToast","sap/ui/model/json/JSONModel"], function(Controller, MessageToast,JSONModel) {
 	"use strict";
 	return Controller.extend("com.myorg.myapp.controller.Main", {
+       onInit:function(){
+		const oModel = new JSONModel();
+		oModel.loadData("localhost:3000/data");
+	   },
 		onPress: function() {
 			MessageToast.show("Hello World");
+		},
+		onAdd:function(){
+			alert("Add button clicked");
 		}
 	});
 })
