ae9ec1e52de163f0f45baece49a1d8b48d2cda3c
Pauline Umunyana <pauline.umunyana@moyotech.solutions>
2024-12-22 00:04:11 +0200

    adding the validation of ID

--- com.myorg.myapp/webapp/controller/Main.controller.js
+++ com.myorg.myapp/webapp/controller/Main.controller.js
@@ -1,4 +1,7 @@
-sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model/json/JSONModel"], function (Controller, MessageToast, JSONModel) {
+sap.ui.define([
+	"sap/ui/core/mvc/Controller", 
+	"sap/m/MessageToast", 
+	"sap/ui/model/json/JSONModel"], function (Controller, MessageToast, JSONModel) {
 	"use strict";
 	return Controller.extend("com.myorg.myapp.controller.Main", {
 		onInit: function () {
@@ -9,6 +12,18 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model
 			// You can directly access the model data if needed
 			var oData = oModel.getData();  // This gets the raw data from the model
 			console.log(oData);
+		},
+		validateProductId: function(oEvent) {
+			const input = oEvent.getSource();
+			const value = input.getValue();
+			
+			if (!/^\d+$/.test(value)) {
+				input.setValueState("Error");
+				return false;
+			}
+			
+			input.setValueState("None");
+			return true;
 		},
 		onPress: function () {
 			MessageToast.show("Hello World");
@@ -129,6 +144,6 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/model
 			}).catch(() => {
 				MessageToast.show("Error updating user");
 			})
-		} 
+		}
 	});
 })
