4b08bc16bd57129777a69f66e745eb83f3cfd288
Pauline Umunyana <pauline.umunyana@moyotech.solutions>
2024-12-22 12:39:02 +0200

    adding update functionality in my application

--- com.myorg.myapp/webapp/controller/Main.controller.js
+++ com.myorg.myapp/webapp/controller/Main.controller.js
@@ -1,153 +1,153 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
 ], function (Controller, MessageToast, JSONModel) {
-    "use strict";
-    return Controller.extend("com.myorg.myapp.controller.Main", {
-        onInit: function () {
-            const oModel = new JSONModel();
-            oModel.loadData("http://localhost:3000/data", null, false);
-            this.getView().setModel(oModel);
-            this._originalData = oModel.getProperty("/users");
-        },
+	"use strict";
+	return Controller.extend("com.myorg.myapp.controller.Main", {
+		onInit: function () {
+			const oModel = new JSONModel();
+			oModel.loadData("http://localhost:3000/data", null, false);
+			this.getView().setModel(oModel);
+			this._originalData = oModel.getProperty("/users");
+		},
 
-        validateProductId: function (oEvent) {
-            const input = oEvent.getSource();
-            const value = input.getValue();
+		validateProductId: function (oEvent) {
+			const input = oEvent.getSource();
+			const value = input.getValue();
 
-            if (!/^\d+$/.test(value)) {
-                input.setValueState("Error");
-                return false;
-            }
+			if (!/^\d+$/.test(value)) {
+				input.setValueState("Error");
+				return false;
+			}
 
-            input.setValueState("None");
-            return true;
-        },
+			input.setValueState("None");
+			return true;
+		},
 
-        onPress: function () {
-            MessageToast.show("Hello World");
-        },
+		onPress: function () {
+			MessageToast.show("Hello World");
+		},
 
-        onAdd: function () {
-            this.byId("creating-dialog").open();
-        },
+		onAdd: function () {
+			this.byId("creating-dialog").open();
+		},
 
-        onCancelNewUser: function () {
-            this.byId("creating-dialog").close();
-        },
+		onCancelNewUser: function () {
+			this.byId("creating-dialog").close();
+		},
 
-        onEdit: function (oEvent) {
-            const button = oEvent.getSource();
-            const listItem = button.getParent();
-            const context = listItem.getBindingContext();
-            const userData = context.getObject();
-            this._selectedUserId = userData.id;
-            const dialog = this.byId("updatingDialog");
-            this.byId("updating-id").setValue(userData.id);
-            this.byId("updating-name").setValue(userData.name);
-            this.byId("updating-email").setValue(userData.email);
-            this.byId("updating-address").setValue(userData.address);
-            this.byId("updating-city").setValue(userData.city);
-            dialog.open();
-        },
+		onEdit: function (oEvent) {
+			const button = oEvent.getSource();
+			const listItem = button.getParent();
+			const context = listItem.getBindingContext();
+			const userData = context.getObject();
+			this._selectedUserId = userData.id;
+			const dialog = this.byId("updatingDialog");
+			this.byId("updating-id").setValue(userData.id);
+			this.byId("updating-name").setValue(userData.name);
+			this.byId("updating-email").setValue(userData.email);
+			this.byId("updating-address").setValue(userData.address);
+			this.byId("updating-city").setValue(userData.city);
+			dialog.open();
+		},
 
-        onCancelUpdatedUser: function () {
-            this.byId("updatingDialog").close();
-        },
+		onCancelUpdatedUser: function () {
+			this.byId("updatingDialog").close();
+		},
 
-        onSaveNewUser: function () {
-            const ID = this.byId("creating-id").getValue();
-            const Name = this.byId("creating-name").getValue();
-            const email = this.byId("creating-email").getValue();
-            const address = this.byId("creating-address").getValue();
-            const city = this.byId("creating-city").getValue();
+		onSaveNewUser: function () {
+			const ID = this.byId("creating-id").getValue();
+			const Name = this.byId("creating-name").getValue();
+			const email = this.byId("creating-email").getValue();
+			const address = this.byId("creating-address").getValue();
+			const city = this.byId("creating-city").getValue();
 
-            fetch("http://localhost:3000/add-user", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    id: ID,
-                    name: Name,
-                    email: email,
-                    address: address,
-                    city: city
-                })
-            }).then(() => {
-                MessageToast.show("User added successfully");
-                this.byId("creating-dialog").close();
-                this._refreshModel();
-            }).catch(() => {
-                MessageToast.show("Error adding user");
-            });
-        },
+			fetch("http://localhost:3000/add-user", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					id: ID,
+					name: Name,
+					email: email,
+					address: address,
+					city: city
+				})
+			}).then(() => {
+				MessageToast.show("User added successfully");
+				this.byId("creating-dialog").close();
+				this._refreshModel();
+			}).catch(() => {
+				MessageToast.show("Error adding user");
+			});
+		},
 
-        onDelete: function (oEvent) {
-            var oItem = oEvent.getSource();
-            var oCtx = oItem.getBindingContext();
-            var sPath = oCtx.getPath();
-            var oModel = this.getView().getModel();
-            var aData = oModel.getProperty("/users");
+		onDelete: function (oEvent) {
+			var oItem = oEvent.getSource();
+			var oCtx = oItem.getBindingContext();
+			var sPath = oCtx.getPath();
+			var oModel = this.getView().getModel();
+			var aData = oModel.getProperty("/users");
 
-            // Create a confirmation dialog
-            var oDialog = new sap.m.Dialog({
-                title: 'Confirm',
-                type: 'Message',
-                content: new sap.m.Text({ text: 'Do you want to delete this user?' }),
-                beginButton: new sap.m.Button({
-                    text: 'Delete',
-                    press: function () {
-                        // Delete the user
-                        aData.splice(sPath.substring(sPath.lastIndexOf('/') + 1), 1);
-                        oModel.setProperty("/users", aData);
-                        oModel.updateBindings();
-                        oDialog.close();
-                    }
-                }),
-                endButton: new sap.m.Button({
-                    text: 'Cancel',
-                    press: function () {
-                        oDialog.close();
-                    }
-                }),
-                afterClose: function () {
-                    oDialog.destroy();
-                }
-            });
+			// Create a confirmation dialog
+			var oDialog = new sap.m.Dialog({
+				title: 'Confirm',
+				type: 'Message',
+				content: new sap.m.Text({ text: 'Do you want to delete this user?' }),
+				beginButton: new sap.m.Button({
+					text: 'Delete',
+					press: function () {
+						// Delete the user
+						aData.splice(sPath.substring(sPath.lastIndexOf('/') + 1), 1);
+						oModel.setProperty("/users", aData);
+						oModel.updateBindings();
+						oDialog.close();
+					}
+				}),
+				endButton: new sap.m.Button({
+					text: 'Cancel',
+					press: function () {
+						oDialog.close();
+					}
+				}),
+				afterClose: function () {
+					oDialog.destroy();
+				}
+			});
 
-            // Open the dialog
-            oDialog.open();
-        },
+			// Open the dialog
+			oDialog.open();
+		},
 
-	
 
-        onSearch: function (oEvent) {
-            const searchValue = oEvent.getParameter("newValue").toLowerCase();
-            const oModel = this.getView().getModel();
-            const aData = this._originalData; // Use the original data
 
-            if (searchValue) {
-                const filteredData = aData.filter(user => {
-                    return ['id', 'name', 'city'].some(key => {
-                        return String(user[key]).toLowerCase().includes(searchValue);
-                    });
-                });
-                oModel.setProperty("/users", filteredData);
-            } else {
-                // If search field is cleared, restore all data
-                oModel.setProperty("/users", aData);
-            }
-        },
+		onSearch: function (oEvent) {
+			const searchValue = oEvent.getParameter("newValue").toLowerCase();
+			const oModel = this.getView().getModel();
+			const aData = this._originalData; // Use the original data
 
-        _refreshModel: function () {
-            const oModel = this.getView().getModel();
-            oModel.loadData("http://localhost:3000/data", null, false);
-            this._originalData = oModel.getProperty("/users");
-        },
-		onSaveUpdatedUser:function(){
-			alert("Save Updated User");
-		}
-    });
+			if (searchValue) {
+				const filteredData = aData.filter(user => {
+					return ['id', 'name', 'city'].some(key => {
+						return String(user[key]).toLowerCase().includes(searchValue);
+					});
+				});
+				oModel.setProperty("/users", filteredData);
+			} else {
+				// If search field is cleared, restore all data
+				oModel.setProperty("/users", aData);
+			}
+		},
+
+		_refreshModel: function () {
+			const oModel = this.getView().getModel();
+			oModel.loadData("http://localhost:3000/data", null, false);
+			this._originalData = oModel.getProperty("/users");
+		},
+		
+	})
+
+
 });
